52b1f5c37c6e1ff6740ddf8f57bbfa5a322491b8
Mohamed Ghoul <118106616+Mohavvvvd@users.noreply.github.com>
2026-04-02 23:11:22 +0100

    Reorder components in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,8 +16,9 @@ const Home = () => {
       <Skills />
       <Experience />
       <Education />
-      <Contact />
+      
       <EngineeringMindset />
+      <Contact />
       <Footer />
     </>
   );
